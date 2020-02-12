bdbee37a9ab6020f662d773581a2ff720efd367a
PhilMcConn <60410681+PhilMcConn@users.noreply.github.com>
2020-02-12 15:37:35 -0600

    Add files via upload

--- useIt.java
+++ useIt.java
@@ -1,27 +1,33 @@
 class Xamples{
 	
-	public String ShowYours() {
-		
+	public String ShowYours() 
+	{
+		return "ShowYours without params";
 	}
 	
-	public String ShowMine() {
-		
+	public String ShowMine() 
+	{
+		return "ShowMine without params";
 	}
 	
-	public int ShowYours(String C) {
-		
+	public int ShowYours(String C) 
+	{
+		return 10;
 	}
 	
-	public int ShowMine(String D) {
-		
+	public int ShowMine(String D) 
+	{
+		return 40;
 	}
 	
-	public double ShowYours(int C) {
-		
+	public double ShowYours(int C) 
+	{
+		 return 60.01;
 	}
 	
-	public double ShowMine(int D) {
-		
+	public double ShowMine(int D) 
+	{
+		return 7.77;
 	}
 
 }
@@ -30,21 +36,27 @@ class useIt{
 	Xamples iWill = new Xamples();
 	
 	int saveInt = 0;
-	int SaveString = "testing";
-	int SaveDouble = 42.42;
+	String SaveString = "testing";
+	double SaveDouble = 42.42;
+	int secondInt = 2;
 	
-	iWill.ShowMine();
 	
-	iWill.ShowYours();
 	
-	iWill.ShowMine("i'm telling mom");
+	String ShowMine2=iWill.ShowMine();
 	
-	iWill.ShowYours("ok");
+	String ShowYours2=iWill.ShowYours();
 	
-	iWill.ShowMine(42);
+	int StrMinewith=iWill.ShowMine("i'm telling mom");
 	
-	iWill.ShowYours(24);
+	int strYourswith=iWill.ShowYours("ok");
+	
+	double ShowMinenum = iWill.ShowMine(42);
+	
+	double ShowYoursnum = iWill.ShowYours(24);
+	
+	public static void main(String args[]){
 	
 	System.out.println();
+	}
 
 }
